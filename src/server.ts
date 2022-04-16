b95d4390f874dc7757d18f8ab4e09866df9449c6
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-04-16 19:43:38 -0300

    feat: integrate pg with docker and run first query

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import { PORT } from 'config/constants';
 import { logger } from 'config/logger';
 import express from 'express';
+import { query } from 'infra/database';
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ message: 'coe' });
+app.get('/', async (req, res) => {
+  const rows = await query(`
+    SELECT * FROM pets`);
+  return res.json(rows);
 });
 
 app.listen(PORT, () => logger.info(`🔥 Server running in http://localhost:${PORT}`));
